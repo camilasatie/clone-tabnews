62cf65b1bf00ce28895c2eb0b9599cdd3c84fab5
Camila Satie <camilasatie.dev@gmail.com>
2024-02-23 18:49:24 +0000

    chore: add `lint:check`and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso.dev para estudo de programação
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return (
-        <h1>"Grandes coisas vêm do trabalho duro e perseverança, sem desculpas."</h1>
-    );
+  return (
+    <h1>
+      "Grandes coisas vêm do trabalho duro e perseverança, sem desculpas."
+    </h1>
+  );
 }
 
 export default Home;
